c24a7bd45d5696fe6918f22cf1f735912f6d9ad6
Med KTITE <ktite.m3@gmail.com>
2024-11-29 12:22:18 +0100

    🔄 Implement ListsComponent & 📦 Ensure necessary modules and components are imported

--- src/app/pages/dashboard/contacts/lists/lists.component.ts
+++ src/app/pages/dashboard/contacts/lists/lists.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [CommonModule, FormsModule, ],
   templateUrl: './lists.component.html',
-  styleUrl: './lists.component.scss'
+  styleUrls: ['./lists.component.scss']
 })
 export class ListsComponent {
+  isOpened = false;
 
+  toggleCreateNewList() {
+    this.isOpened = !this.isOpened;
+  }
 }
