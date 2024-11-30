d71ccda14cc8a908907ce776c08a76bfd29d3217
Med KTITE <ktite.m3@gmail.com>
2024-11-30 10:32:05 +0100

    ✨ Update the e-mailing feature and ensure the UI improvements

--- src/app/pages/dashboard/campaigns/emails/emails.component.ts
+++ src/app/pages/dashboard/campaigns/emails/emails.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+
 @Component({
   selector: 'app-emails',
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule,],
   templateUrl: './emails.component.html',
   styleUrl: './emails.component.scss'
 })
