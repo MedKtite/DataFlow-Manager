137aa3369b10af549a802b01af731901a42a2459
Med KTITE <ktite.m3@gmail.com>
2024-12-08 22:01:46 +0100

    ✨ Enhance web scraping component with structured layout and data extraction options

--- FrontEnd/src/app/pages/dashboard/web-scraping/web-scraping.component.ts
+++ FrontEnd/src/app/pages/dashboard/web-scraping/web-scraping.component.ts
@@ -1,11 +1,17 @@
+// src/app/web-scraping/web-scraping.component.ts
 import { Component } from '@angular/core';
+import { faDotCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-web-scraping',
-  imports: [],
   templateUrl: './web-scraping.component.html',
-  styleUrl: './web-scraping.component.scss'
+  styleUrls: ['./web-scraping.component.scss']
 })
 export class WebScrapingComponent {
+  faDotCircle = faDotCircle;
 
+  toggleCheckbox(id: string): void {
+    const checkbox = document.getElementById(id) as HTMLInputElement;
+    checkbox.checked = !checkbox.checked;
+  }
 }
